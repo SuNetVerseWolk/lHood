dda75631eb3242272373ae9ab70cc416b3c0d760
SuNetVerseWolk <volk333333333@gmail.com>
2024-01-31 18:15:39 +0300

    Made Item's animations

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,27 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 import Navbar from '../layouts/Navbar'
-import Avatar from './Avatar';
+import Avatar from './Avatar'
+import ItemMenuSvg from '../assets/itemMenu.svg?react'
 
 const Item = ({children, id, name, img}) => {
 	const navigate = useNavigate();
+	let [holded, setHolded] = useState(false);
 
 	return (
-		<div
+		<motion.div
 			className='item'
+			animate={{scaleX: 1}}
+			initial={{scaleX: 0, transformOrigin: children ? 'center left' : 'center right'}}
+			transition={{ease: 'easeOut'}}
+			
 			onClick={(e) => navigate(id)}
+			onTouchStart={e => setTimeout(e => setHolded(true), 500)}
+			onTouchEnd={e => setTimeout(e => setHolded(false), 5000)}
 		>
-			<div id="main">
+			<motion.div id="main" transition={{ease: 'backInOut'}}>
 				{img && <Avatar src={img} href='' />}
 				<p id='name'>{name}</p>
-			</div>
+			</motion.div>
+
+			{console.log(holded)}
 			{
-				children &&
-				<Navbar>
-					{children}
-				</Navbar>
+				<motion.div
+					animate={{visibility: 'visible', scaleX: 1}}
+					transition={{delay: .3, ease: 'backOut'}}
+					initial={{
+						visibility: 'hidden',
+						scaleX: 0,
+						transformOrigin: 'center right'
+					}}>
+					<Navbar>
+						{children || !holded ?
+							children :
+							<motion.div
+								animate={{scaleX: 1}}
+								transition={{delay: .3, ease: 'backOut'}}
+								initial={{
+									scaleX: 0,
+									transformOrigin: 'center right'
+								}}
+								onClick={e => {
+								e.stopPropagation();
+							}}>
+								<ItemMenuSvg />
+							</motion.div>
+						}
+					</Navbar>
+				</motion.div>
 			}
-		</div>
+		</motion.div>
 	)
 }
 
